0aaf6d1af737c22bacc6764619a2c805c6eba477
Aditya Raj <adityaraj93@gmail.com>
2016-03-17 11:46:36 +0530

    Arrangement

--- ipt.sh
+++
@@ -1,8 +0,0 @@
-# Run this on the host linux system
-
-#IP address of the container used to sniff and analyse the packets
-echo "Sniffer's address: "
-read sn_ip
-
-sudo iptables -I FORWARD -i docker0 -j TEE --gateway $sn_ip
-sudo iptables -I FORWARD -o docker0 -j TEE --gateway $sn_ip
